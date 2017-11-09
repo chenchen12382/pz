45926ae3020d6caab05c380ebb812dac3276454b
zhanglb913 <dream91393@outlook.com>
2017-11-09 18:00:43 +0800

    协议编号&收据编号

--- src/main/java/com/fh/controller/ProtocolNumController.java
+++ src/main/java/com/fh/controller/ProtocolNumController.java
@@ -1,11 +1,16 @@
 package com.fh.controller;
-
 import com.fh.base.BaseController;
+import com.fh.base.BaseQuery;
+import com.fh.base.ResultInfo;
+import com.fh.model.ProtocolNum;
 import com.fh.service.ProtocolNumService;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2017/11/9.
@@ -14,18 +19,92 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("protocol")
 public class ProtocolNumController extends BaseController{
 
-//    @Autowired
-//    private ProtocolNumService protocolNumService;
+    @Autowired
+    private ProtocolNumService protocolNumService;
 
 
     @RequestMapping("index/{type}")
     public String index(@PathVariable Integer type){
         switch (type){
             default:
-                return "xybh";
-            case 2 :
                 return "sjbh";
+            case 2 :
+                return "xybh";
         }
     }
+    
+    /*收据编号*/
+    @RequestMapping("list1")
+	@ResponseBody
+	public Map<String, Object> selectForPage1(BaseQuery query) {
+		Map<String, Object> result = protocolNumService.selectForPage1(query);
+		return result;
+	}
+
+	@RequestMapping("find_all1")
+	@ResponseBody
+	public List<ProtocolNum> findAll1() {
+		Map<String, Object> result = protocolNumService.selectAll1();
+		return (List<ProtocolNum>) result.get("rows");
+
+	}
+	
+	@RequestMapping("delete1")
+	@ResponseBody
+	public ResultInfo deleteBatch(String ids) {
+		protocolNumService.deleteBatch1(ids);
+		return success("删除成功");
+	}
+/*
+	@RequestMapping("add1")
+	@ResponseBody
+	public ResultInfo insert1(ProtocolNum protocolNum) {
+		protocolNumService.insert1(protocolNum);
+		return success("添加成功");
+	}
+
+	@RequestMapping("update1")
+	@ResponseBody
+	public ResultInfo update1(ProtocolNum protocolNum) {
+		protocolNumService.update1(protocolNum);
+		return success("修改成功");
+	}
+*/
+	
+	/*协议编号*/
+	  @RequestMapping("list2")
+		@ResponseBody
+		public Map<String, Object> selectForPage2(BaseQuery query) {
+			Map<String, Object> result = protocolNumService.selectForPage2(query);
+			return result;
+		}
+
+		@RequestMapping("find_all2")
+		@ResponseBody
+		public List<ProtocolNum> findAll2() {
+			Map<String, Object> result = protocolNumService.selectAll2();
+			return (List<ProtocolNum>) result.get("rows");
+		}
+		
+		@RequestMapping("delete2")
+		@ResponseBody
+		public ResultInfo deleteBatch2(String ids) {
+			protocolNumService.deleteBatch2(ids);
+			return success("删除成功");
+		}
+
+	/*	@RequestMapping("add2")
+		@ResponseBody
+		public ResultInfo insert2(ProtocolNum protocolNum) {
+			protocolNumService.insert2(protocolNum);
+			return success("添加成功");
+		}
+
+		@RequestMapping("update2")
+		@ResponseBody
+		public ResultInfo update2(ProtocolNum protocolNum) {
+			protocolNumService.update2(protocolNum);
+			return success("修改成功");
+		}*/
 
 }
--- src/main/java/com/fh/dao/ProtocolNumDao.java
+++ src/main/java/com/fh/dao/ProtocolNumDao.java
@@ -1,7 +1,60 @@
 package com.fh.dao;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import com.fh.model.ProtocolNum;
+import com.fh.model.UpLog;
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
 
 /**
  * Created by Administrator on 2017/11/9.
  */
-public class ProtocolNumDao {
+public interface ProtocolNumDao {
+
+    @Select("select id, center_id,sjbh,create_date,update_date from t_sjbh where is_valid = 1" + " order by id ")
+    PageList<ProtocolNum> selectAll1(PageBounds pageBounds);
+    
+    @Select("select id, center_id,xybh,create_date,update_date from t_xybh where is_valid = 1" + " order by id ")
+    PageList<ProtocolNum> selectAll2(PageBounds pageBounds);
+
+    /*
+    @Insert("insert into t_sjbh (center_id,sjbh,is_valid,create_date,update_date) values " +
+            " ( #{centerId},#{sjbh},1,now(),now() )")
+    void insert1(ProtocolNum protocolNum);
+    @Insert("insert into t_sjbh (center_id,sjbh,is_valid,create_date,update_date) values " +
+            " ( #{centerId},#{sjbh},1,now(),now() )")
+    void insert2(ProtocolNum protocolNum);
+    @Insert("insert into t_xybh (center_id,xybh,is_valid,create_date,update_date) values " +
+            " ( #{centerId},#{xybh},1,now(),now() )")*/
+  /*  
+    @Update("update t_sjbh set center_id=#{centerId},sjbh=#{sjbh},update_date=now() where id= #{id} ")
+    void update1(ProtocolNum protocolNum);
+    @Update("update t_xybh set center_id=#{centerId},xybh=#{xybh},update_date=now() where id= #{id} ")
+    void update2(ProtocolNum protocolNum);*/
+    
+    
+
+    @Update("update t_sjbh set is_valid = 0 , update_date=now()  where id in (${ids})")
+    void deleteBatch1(@Param("ids") String ids);
+    @Update("update t_xybh set is_valid = 0 , update_date=now()  where id in (${ids})")
+    void deleteBatch2(@Param("ids") String ids);
+
+        
+    @Select("select id,center_id,sjbh,create_date,update_date from t_sjbh where is_valid=1" + " order by id  ")
+    List<ProtocolNum> selectSjbh();
+    @Select("select id,center_id,xybh,create_date,update_date from t_xybh where is_valid=1" + " order by id  ")
+    List<ProtocolNum> selectXybh();
+    
+    
+    
+    
+  /*    
+    @Select("select sjbh from t_sjbh where sjbh = #{sjbh} and is_valid = 1 ")
+    String findBySjbh(@Param("sjbh") String sjbh); 
+    @Select("select sjbh from t_xybh where xybh = #{xybh} and is_valid = 1 ")
+    String findByXybh(@Param("sjbh") String xybh);*/
 }
